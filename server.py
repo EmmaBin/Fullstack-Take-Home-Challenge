32677d852d75a38c7c946439fc56a928c372f9ec
emmacodes <binma.emma@gmail.com>
2022-10-26 18:33:06 -0700

    learned that I need to put html inside the templates folder, fixed render homepage bug

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 from sre_constants import SUCCESS
 from flask import Flask, render_template, request, flash, session, redirect, jsonify, json
-from model import connect_to_db, db
-import crud
-import requests
-import os
+from jinja2 import StrictUndefined
+# from model import connect_to_db, db
+# import crud
+# import requests
+# import os
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
+#render homepage
+@app.route("/")
+def homepage():
+    """View homepage."""
+    
+    return render_template("index.html")
 
 
 
@@ -17,5 +26,5 @@ app = Flask(__name__)
 
 
 if __name__ == "__main__":
-    connect_to_db(app)
+    # connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
